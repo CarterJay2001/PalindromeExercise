fec1a70ba897322d32da5284fb321c92b10e3d80
Jaylon Carter <carterjay2001@outlook.com>
2022-02-02 02:42:58 -0600

    initial commit

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+      public bool IsAPalindrome(string word)
+      {
+            char[] charArray = word.ToCharArray();
+            Array.Reverse(charArray);
+            return word == new string(charArray);
+      }
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -5,10 +5,21 @@ namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("car", false)]
+        [InlineData("hannah", true)]
+        [InlineData("cardio", false)]
+        [InlineData("madam", true)]
+        public void PalindromeTest(string word, bool expected)
         {
+            //Arrange
+            var wordSmith = new WordSmith();
 
+            //Act
+            bool ans = wordSmith.IsAPalindromeTest(word);
+
+            //Assert
+            Assert.Equal(expected, ans);
         }
     }
 }
